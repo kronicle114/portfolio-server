5cfead1ab69cdcd8800ebb37eaf45eb947a54a18
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-08-07 22:26:01 -0400

    added packages

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+// get our client url, get our database configs/url, and your port
+
+// build an express app
+const app = express()
+
+// log all stuff
+
+// do cors stuff
+
+// load your public assets
+
+// body parser
+
+// api endpoints
+
+//error handler
+
+// add db listener
+
+// export app for testing
